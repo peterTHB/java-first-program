51b62609845e172dc958a89bd888582c42cdd832
PTHBui <tienb3774@gmail.com>
2022-10-20 14:56:55 +1100

    Mortgage and Savings updated with utilities

--- src/main/java/com/h2/SavingsCalculator.java
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -18,11 +18,11 @@ public class SavingsCalculator {
         float[] debits = new float[debitsAsString.length];
 
         for (int i=0; i < creditsAsString.length ; i++) {
-            credits[i] = Float.parseFloat(creditsAsString[i]);
+            credits[i] = Utilities.getFloatValue(creditsAsString[i]);
         }
 
         for (int i=0; i < debitsAsString.length ; i++) {
-            debits[i] = Float.parseFloat(debitsAsString[i]);
+            debits[i] = Utilities.getFloatValue(debitsAsString[i]);
         }
 
         SavingsCalculator calculator = new SavingsCalculator(credits, debits);
